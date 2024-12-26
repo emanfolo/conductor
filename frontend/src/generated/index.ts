c57313988f06fc75d18557a11a6965b0b5ed181b
emanfolo <emmanuelfolorunsho@icloud.com>
2024-12-26 10:14:49 +0000

    Remove type generation in favour of manual typing

--- frontend/src/generated/index.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type { PrimeCalculationInput } from './models/PrimeCalculationInput';
-export type { PrimeCalculationProgress } from './models/PrimeCalculationProgress';
-export type { PrimeCalculationResult } from './models/PrimeCalculationResult';
